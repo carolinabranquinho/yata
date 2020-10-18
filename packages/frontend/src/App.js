a284177ae57b7c1758ec7a66579cfd5d19a0f0a5
Carolina Branquinho <carol.a.branquinho@gmail.com>
2020-10-18 09:22:16 -0300

    add tasks route

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import UsersPage from "./pages/users/UsersPage";
+import TasksList from ".pages/list/UserTaskList";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         </div>
       </Route>
       <Route path="/users" component={UsersPage} />
+      <Route path="users/:id/tasks" component={TasksList} />
     </BrowserRouter>
   );
 }
